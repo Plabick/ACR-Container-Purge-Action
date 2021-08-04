82838ea4ac6a89fe82abe84e54b6df673035c6d8
Peter Labick <Plabick@users.noreply.github.com>
2021-08-04 11:25:23 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 set -e # Abort at first error
-pwsh /purge.ps1 -registry $1 -username $2 -tenant $3 -password $4 -repo $5 -tagRegex $6 -repoRegex $7 -daysToKeep $8 -keep $9
+pwsh /purge.ps1 -registry $1 -username $2 -tenant $3 -password $4 -repoTarget $5 -tagRegex $6 -repoRegex $7 -daysToKeep $8 -keep $9
