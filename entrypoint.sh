4e92f82672198c4f1f93ab8355f32ce2b6f387b5
Peter Labick <Plabick@users.noreply.github.com>
2021-08-05 12:44:08 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 set -e # Abort at first error
+echo "Running \"pwsh /purge.ps1 -registry "$1" -username "$2" -tenant "$3" -password "$4" -repoTarget "$5" -tagRegex "$6" -repoRegex "$7" -daysToKeep "$8" -keep "$9" -dryRun "True"\""
 pwsh /purge.ps1 -registry "$1" -username "$2" -tenant "$3" -password "$4" -repoTarget "$5" -tagRegex "$6" -repoRegex "$7" -daysToKeep "$8" -keep "$9" -dryRun "True"
