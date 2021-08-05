8ddd7aabf10a2c26d6d08e9f1e9fb8362256a1f0
Peter Labick <Plabick@users.noreply.github.com>
2021-08-05 13:53:26 -0400

    Update purge.ps1

--- purge.ps1
+++ purge.ps1
@@ -1,32 +1,36 @@
 param (
-	[Parameter(Mandatory = $false)]$registry = $env:registry,
-	[Parameter(Mandatory = $false)]$username = $env:username ,
-	[Parameter(Mandatory = $false)]$tenant = $env:tenant,
-	[Parameter(Mandatory = $false)]$password = $env:password,
-	[Parameter(Mandatory = $false)]$repoTarget = $env:repo,
-	[Parameter(Mandatory = $false)]$tagRegex = $env:tagRegex,
-	[Parameter(Mandatory = $false)]$repoRegex = $env:repoRegex ,
-	[Parameter(Mandatory = $false)]$daysToKeep = $env:daysToKeep,
-	[Parameter(Mandatory = $false)]$keep = $env:keep,
-	[Parameter(Mandatory = $false)]$dryRun = $env:dryRun
+    [Parameter(Mandatory = $false)]$registry = $env:registry,
+    [Parameter(Mandatory = $false)]$username = $env:username ,
+    [Parameter(Mandatory = $false)]$tenant = $env:tenant,
+    [Parameter(Mandatory = $false)]$password = $env:password,
+    [Parameter(Mandatory = $false)]$repoTarget = $env:repo,
+    [Parameter(Mandatory = $false)]$tagRegex = $env:tagRegex,
+    [Parameter(Mandatory = $false)]$repoRegex = $env:repoRegex ,
+    [Parameter(Mandatory = $false)]$daysToKeep = $env:daysToKeep,
+    [Parameter(Mandatory = $false)]$keep = $env:keep,
+    [Parameter(Mandatory = $false)]$dryRun = $env:dryRun,
+    [Parameter(Mandatory = $false)]$deleteUntagged = $env:deleteUntagged 
 )
 az login --service-principal -u $username -p $password --tenant $tenant
 Write-Host "-----Logged In -----"
 If ($dryRun -eq $true) {
-echo "----- Conducting Dry Run -----"
-$dryRunCommand = "--dry-run"
-} Else {
-$dryRunCommand = ""}
+    Write-Output "-----Dry Run-----"
+    $dryRunCommand = "--dry-run"
+}
+Else {
+    $dryRunCommand = ""
+}
+$deleteUntaggedCommand = If ($deleteUntagged -eq $true) { "--untagged" } { "" }
 if ($repoTarget -eq "all") {
-	$REPOS = az acr repository list -n $registry  | Where-Object { ($_ –ne "[") -and ($_ –ne "]") }
-	foreach ($REPO in $REPOS) {
-		if ($REPO -match $repoRegex) {
-			$REPO = $REPO.Replace(" ", "").Replace(",", "")
-			Write-Output "----- Purging $REPO -----"
-			az acr run --registry $registry --cmd "acr purge --ago ${daysToKeep}d --keep $keep --filter '${REPO}:$tagRegex' $dryRunCommand" /dev/null
-		}	
-	}
+    $REPOS = az acr repository list -n $registry  | Where-Object { ($_ –ne "[") -and ($_ –ne "]") }
+    foreach ($REPO in $REPOS) {
+        if ($REPO -match $repoRegex) {
+            $REPO = $REPO.Replace(" ", "").Replace(",", "")
+            Write-Output "----- Purging $REPO -----"
+            az acr run --registry $registry --cmd "acr purge --ago ${daysToKeep}d --keep $keep --filter '${REPO}:$tagRegex' $dryRunCommand $deleteUntaggedCommand" /dev/null
+        }	
+    }
 }
 else {
-	az acr run --registry $registry --cmd "acr purge --ago ${daysToKeep}d --keep $keep --filter '${repoTarget}:$tagRegex' $dryRunCommand" /dev/null
+    az acr run --registry $registry --cmd "acr purge --ago ${daysToKeep}d --keep $keep --filter '${repoTarget}:$tagRegex' $dryRunCommand $deleteUntaggedCommand" /dev/null
 }
